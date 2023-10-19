84e7fc70ae6b39eb02cfd785dbec798ffe6e21f6
MarisolAr30 <148324162+MarisolAr30@users.noreply.github.com>
2023-10-19 09:23:00 +0200

    add exercise 01

---
+++ src/main/java/com/campusdual/exercise01.java
@@ -0,0 +1,27 @@
+package com.campusdual;
+
+public class exercise01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String cnv = "200";
+        Integer cnvInt = Integer.parseInt(cnv);
+        cnv = cnv +1;
+        cnvInt = cnvInt +1;
+        String cnvStr = String.valueOf(cnvInt);
+        char cnvChar = cnvStr.charAt(2);
+
+        int i = 1;
+        String iStr = String.valueOf (i);
+        Double iDouble = Double.valueOd(iStr);
+        String iDStr = String.valueOf(iDouble);
+        System.out.println(Boolean.valueOf(iDStr));
+        Sytem.out.println(Sring
+
+
+        // 1 = "1" = 1.0 = true
+        // 0 = "0" = 0.0 = false
+        // 2 = "2" = 2.00000 = true = "true"
+    }
+
+}
