1e81116f479ebf63389e8e14610778f7ed2cb4e8
MarisolAr30 <148324162+MarisolAr30@users.noreply.github.com>
2023-10-19 10:32:12 +0200

    add Exercise03

---
+++ src/main/java/com/campusdual/Exercise03.java
@@ -0,0 +1,18 @@
+package com.campusdual;
+
+public class Exercise03 {
+
+    public static String saludo(String nombre){
+        String mensaje = "Hola. Bienvenido, "+ nombre;
+        return mensaje;
+    }
+
+    public static void fechaActual(String fecha){
+        System.out.println(fecha);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(saludo("Marisol"));
+        fechaActual("19/10/2023");
+    }
+}
